072496cadb5a00c3459a17d10acd2ed715625fcc
hridesh <hridesh699@gmail.com>
2024-04-06 21:24:07 +0530

    fix: removed unutilised endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from api import views
 
 userpatterns = [
-    path("users", views.UserList.as_view()),
-    path("user/<int:pk>/", views.UserDetail.as_view()),
     path("register", views.UserRegister.as_view()),
     path("login", views.UserLogin.as_view()),
     path("logout", views.UserLogout.as_view()),
