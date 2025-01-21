7d4888d11cae0948f50d7c6bdf8811d630ed4ded
Hridesh MG <hridesh699@gmail.com>
2025-01-21 19:11:33 +0530

    feat: implement new auth system

--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 import datetime
 from collections import defaultdict
 
-from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.models import User
-from django.db import IntegrityError
 from django.db.models import F, Max
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions, status
@@ -11,6 +10,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from knox.views import LoginView, LogoutView
 
 from api.models import Collection, Course, Schedule, Session
 from api.serializers import (
@@ -43,36 +45,20 @@ class UserRegister(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserLogin(APIView):
-    def post(self, request, format=None):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            try:
-                token = Token.objects.create(user=user)
-            except IntegrityError:
-                return Response(
-                    {"User is already logged in"}, status=status.HTTP_403_FORBIDDEN
-                )
-            return Response({"token": token.key}, status=status.HTTP_202_ACCEPTED)
-        else:
-            return Response(
-                {"Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED
-            )
-
-
-class UserLogout(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+class UserLogin(LoginView):
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, format=None):
-        try:
-            request.user.auth_token.delete()
-        except Token.DoesNotExist:
-            return Response(
-                {"Token does not exist"}, status=status.HTTP_400_BAD_REQUEST
-            )
-        return Response({"Successfully logged out"}, status=status.HTTP_200_OK)
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        login(request, user)
+        return super().post(request, format=None)
+
+
+class UserLogout(LogoutView):
+    def get_post_response(self, request):
+        return Response({"bye-bye": request.user.username}, status=200)
 
 
 class CollectionView(generics.RetrieveUpdateDestroyAPIView, generics.CreateAPIView):
