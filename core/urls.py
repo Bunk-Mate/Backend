a608665359c511db55b433f17eeaf32939e46d0b
Hridesh MG <hridesh699@gmail.com>
2025-09-22 21:18:29 +0530

    implement sentry

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("api.urls")),
     path("healthcheck/", include("health_check.urls")),
+    path("sentry-debug/", trigger_error),
 ]
